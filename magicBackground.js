54fc1cfb8d8dee17a6be29a156f1185ec445a88f
Pontus Sandström <pontus@avtre.se>
2016-09-20 15:25:11 +0200

    Works alot better when the container is box shaped instead of a whole hero. Applications could be to have all pictures moving, like in harry potter.
    Signed-off-by: Pontus Sandström <pontus@avtre.se>

--- magicBackground.js
+++ magicBackground.js
@@ -1,8 +1,51 @@
+var elements = document.getElementsByClassName("video-background");
+
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	var elements = document.getElementsByClassName("video-overlay");
 	if(elements.length > 0) {
 		for (i = 0, elmLength = elements.length; i < elmLength; i++) { 
 		    elements[i].innerHTML = "";
 		}
 	}
 }
+
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+var resize = function(event) {
+	if(elements.length > 0) {
+		for (i = 0, elmLength = elements.length; i < elmLength; i++) {
+			var videoElement = elements[i].getElementsByTagName("VIDEO")[0];
+
+			var videoDimensions = {
+				"height" : videoElement.videoHeight, 
+				"width" : videoElement.videoWidth
+			};
+
+			var containerDimensions = {
+				"height" : elements[i].clientHeight, 
+				"width" : elements[i].clientWidth
+			};
+
+			if(videoDimensions.width / videoDimensions.height >= containerDimensions.width / containerDimensions.height) {
+				elements[i].classList.remove("set-height", "set-width");
+		    	elements[i].className += " set-height";
+			}
+			else {
+				elements[i].classList.remove("set-height", "set-width");
+		    	elements[i].className += " set-width";
+			}
+		}
+	}
+};
+
+addEvent(window, "resize", resize);
+addEvent(window, "load", resize);
