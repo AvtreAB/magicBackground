bafc3d2d09cb7f86972a3a10348524d9444672a4
Pontus Sandström <pontus@avtre.se>
2016-12-18 20:32:57 +0100

    Big remake for the magicbackground plugin. Should work flawless now.
    Signed-off-by: Pontus Sandström <pontus@avtre.se>

--- magicBackground.js
+++ magicBackground.js
@@ -1,15 +1,27 @@
-var elements = document.getElementsByClassName("video-background");
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	if(elements.length > 0) {
-		for (i = 0, elmLength = elements.length; i < elmLength; i++) { 
-		    elements[i].innerHTML = "";
+var MagicBackground = function() {
+	this.elements = document.getElementsByClassName("video-background");
+	
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		if(this.elements.length > 0) {
+			for (i = 0, elmLength = elements.length; i < elmLength; i++) { 
+				this.elements[i].innerHTML = "";
+			}
 		}
 	}
+	
+	for (i = 0, elmLength = this.elements.length; i < elmLength; i++) {
+		var videoElement = this.elements[i].getElementsByTagName("VIDEO")[0];
+		
+		if(videoElement) {
+			this.addEvent(videoElement, "loadedmetadata", this.resize.bind(this));
+		}
+	}
+	
+	this.addEvent(window, "resize", this.resize.bind(this));
 }
 
-var addEvent = function(object, type, callback) {
-    if (object == null || typeof(object) == 'undefined') return;
+MagicBackground.prototype.addEvent = function(object, type, callback) {
+	if (object == null || typeof(object) == 'undefined') return;
     
     if (object.addEventListener) {
         object.addEventListener(type, callback, false);
@@ -18,41 +30,36 @@ var addEvent = function(object, type, callback) {
     } else {
         object["on"+type] = callback;
     }
-};
+}
 
-var resize = function(event) {
-	if(elements.length > 0) {
-		for (i = 0, elmLength = elements.length; i < elmLength; i++) {
-			var videoElement = elements[i].getElementsByTagName("VIDEO")[0];
-
-			var videoDimensions = {
-				"height" : videoElement.videoHeight, 
-				"width" : videoElement.videoWidth
-			};
-
-			var containerDimensions = {
-				"height" : elements[i].clientHeight, 
-				"width" : elements[i].clientWidth
-			};
-
-			if(videoDimensions.width / videoDimensions.height >= containerDimensions.width / containerDimensions.height) {
-				elements[i].classList.remove("set-height", "set-width");
-		    	elements[i].className += " set-height";
-			}
-			else {
-				elements[i].classList.remove("set-height", "set-width");
-		    	elements[i].className += " set-width";
+MagicBackground.prototype.resize = function(event) {
+	if(this.elements.length > 0) {
+		for (i = 0, elmLength = this.elements.length; i < elmLength; i++) {
+			var containerElement = this.elements[i];
+			var videoElement = containerElement.getElementsByTagName("VIDEO")[0];
+			
+			if(videoElement) {			
+				var videoDimensions = {
+					"height" : videoElement.videoHeight, 
+					"width" : videoElement.videoWidth
+				};
+				console.log(videoDimensions);
+				var containerDimensions = {
+					"height" : containerElement.clientHeight, 
+					"width" : containerElement.clientWidth
+				};
+				console.log(containerDimensions);
+				containerElement.classList.remove("set-height", "set-width");
+	
+				if(videoDimensions.width / videoDimensions.height >= containerDimensions.width / containerDimensions.height) {
+					containerElement.className += " set-height";
+					console.log("Setting height");
+				}
+				else {
+					containerElement.className += " set-width";
+					console.log("Setting width");
+				}
 			}
 		}
 	}
 };
-
-var onResize = function(event) {
-	resize(event);
-	setTimeout(function(){
-		resize(event);
-	}, 50)
-};
-
-addEvent(window, "resize", onResize);
-addEvent(window, "load", onResize);;
